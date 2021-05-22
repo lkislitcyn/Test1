4e5e8348ab1d6affd48ce4562d67790757b67dc3
kila1986 <kluchnik2004@mail.ru>
2021-05-23 00:42:07 +0800

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int ArrayTo[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+	int ArrayTo[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
 	//System.out.println(c=a+b);
 	Solution test = new Solution();
-	int BackArr[]=test.twoSum(ArrayTo,29);
+	int BackArr[]=test.twoSum(ArrayTo,60);
 	System.out.println (BackArr[0]+" "+BackArr[1]); //поставить проверку на null
     }
 }
@@ -16,7 +16,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int i,j,ArrayBack[]=new int[2];
         if (nums == null || nums.length == 0) {
-            return null;
+            return ArrayBack;
         } else {
         for (i=0;i<nums.length-1;i++)
         for (j=0;j<nums.length;j++)
@@ -24,6 +24,6 @@ class Solution {
             if ((nums[i]+nums[j])==target) {ArrayBack[0]=i;ArrayBack[1]=j;return ArrayBack; }
 
         }
-    return null;
+    return ArrayBack;
     }
 }
